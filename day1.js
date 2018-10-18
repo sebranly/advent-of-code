f08be1c42f71ff73ebe9512d606a622e1f5dc333
sebranly <sebranly.pinf@gmail.com>
2018-10-17 23:35:55 -0700

    feat: add day 1 part 2

--- day1.js
+++ day1.js
@@ -7,7 +7,7 @@ const runTests = (data) => {
 			let errorCount = 0;
 			const partKey = `part${partNumber}`;
 			dataDay[partKey].map((dataLine, lineIndex) => {
-				const result = solvers[0](dataLine.input);
+				const result = solvers[0](dataLine.input)[partKey];
 				if (result !== dataLine.output) {
 					console.log(`Expected ${dataLine.output} but got ${result}`);
 					errorCount++;
@@ -17,7 +17,7 @@ const runTests = (data) => {
 				console.log(`Failure: ${errorCount} error(s)`);
 			}
 			else {
-				console.log(`Success! (${dataDay[partKey].length} tests)`);
+				console.log(`Success! (${dataDay[partKey].length} test(s))`);
 			}
 			console.log('');
 		});
@@ -27,6 +27,10 @@ const runTests = (data) => {
 const day1 = (input) => {
 	let coord = { x: 0, y: 0 };
 	let vector = { x: 0, y: -1 };
+
+	let firstLocationVisitedTwice = null;
+	const history = [{ x: 0, y: 0 }];
+
 	const steps = input.split(',').map((step) => step.trim());
 	steps.map((step) => {
 		const letter = step[0];
@@ -48,11 +52,34 @@ const day1 = (input) => {
 			vector.x = 0;
 			vector.y = sign;
 		}
-		coord.x += number * vector.x;
-		coord.y += number * vector.y;
+
+		for (i = 0 ; i < number ; i++) {
+			coord.x += vector.x;
+			coord.y += vector.y;
+
+			if (!firstLocationVisitedTwice) {
+				history.map((historyLine) => {
+					if (historyLine.x === coord.x && historyLine.y === coord.y) {
+						firstLocationVisitedTwice = { ...coord };
+					}
+				});
+			}
+
+			history.push({ ...coord });
+		}
 	});
-	return Math.abs(coord.x) + Math.abs(coord.y);
+
+	const part2 = firstLocationVisitedTwice
+		? Math.abs(firstLocationVisitedTwice.x) + Math.abs(firstLocationVisitedTwice.y)
+		: 0;
+
+	return {
+		part1: Math.abs(coord.x) + Math.abs(coord.y),
+		part2
+	};
 };
+
+const day1Input = 'R4, R1, L2, R1, L1, L1, R1, L5, R1, R5, L2, R3, L3, L4, R4, R4, R3, L5, L1, R5, R3, L4, R1, R5, L1, R3, L2, R3, R1, L4, L1, R1, L1, L5, R1, L2, R2, L3, L5, R1, R5, L1, R188, L3, R2, R52, R5, L3, R79, L1, R5, R186, R2, R1, L3, L5, L2, R2, R4, R5, R5, L5, L4, R5, R3, L4, R4, L4, L4, R5, L4, L3, L1, L4, R1, R2, L5, R3, L4, R3, L3, L5, R1, R1, L3, R2, R1, R2, R2, L4, R5, R1, R3, R2, L2, L2, L1, R2, L1, L3, R5, R1, R4, R5, R2, R2, R4, R4, R1, L3, R4, L2, R2, R1, R3, L5, R5, R2, R5, L1, R2, R4, L1, R5, L3, L3, R1, L4, R2, L2, R1, L1, R4, R3, L2, L3, R3, L2, R1, L4, R5, L1, R5, L2, L1, L5, L2, L5, L2, L4, L2, R3';
 
 const data = [
 	// Day 1
@@ -62,12 +89,16 @@ const data = [
 			{ input: 'R2, R2, R2', output: 2 },
 			{ input: 'R5, L5, R5, R3', output: 12 },
 			{
-				input: 'R4, R1, L2, R1, L1, L1, R1, L5, R1, R5, L2, R3, L3, L4, R4, R4, R3, L5, L1, R5, R3, L4, R1, R5, L1, R3, L2, R3, R1, L4, L1, R1, L1, L5, R1, L2, R2, L3, L5, R1, R5, L1, R188, L3, R2, R52, R5, L3, R79, L1, R5, R186, R2, R1, L3, L5, L2, R2, R4, R5, R5, L5, L4, R5, R3, L4, R4, L4, L4, R5, L4, L3, L1, L4, R1, R2, L5, R3, L4, R3, L3, L5, R1, R1, L3, R2, R1, R2, R2, L4, R5, R1, R3, R2, L2, L2, L1, R2, L1, L3, R5, R1, R4, R5, R2, R2, R4, R4, R1, L3, R4, L2, R2, R1, R3, L5, R5, R2, R5, L1, R2, R4, L1, R5, L3, L3, R1, L4, R2, L2, R1, L1, R4, R3, L2, L3, R3, L2, R1, L4, R5, L1, R5, L2, L1, L5, L2, L5, L2, L4, L2, R3',
+				input: day1Input,
 				output: 161
 			}
 		],
 		part2: [
-			{ input: 'R8, R4, R4, R8', output: 4 }
+			{ input: 'R8, R4, R4, R8', output: 4 },
+			{
+				input: day1Input,
+				output: 110
+			}
 		]
 	}
 ];
@@ -78,6 +109,5 @@ const solvers = [
 
 runTests(data);
 
-const dataDay1 = 'R4, R1, L2, R1, L1, L1, R1, L5, R1, R5, L2, R3, L3, L4, R4, R4, R3, L5, L1, R5, R3, L4, R1, R5, L1, R3, L2, R3, R1, L4, L1, R1, L1, L5, R1, L2, R2, L3, L5, R1, R5, L1, R188, L3, R2, R52, R5, L3, R79, L1, R5, R186, R2, R1, L3, L5, L2, R2, R4, R5, R5, L5, L4, R5, R3, L4, R4, L4, L4, R5, L4, L3, L1, L4, R1, R2, L5, R3, L4, R3, L3, L5, R1, R1, L3, R2, R1, R2, R2, L4, R5, R1, R3, R2, L2, L2, L1, R2, L1, L3, R5, R1, R4, R5, R2, R2, R4, R4, R1, L3, R4, L2, R2, R1, R3, L5, R5, R2, R5, L1, R2, R4, L1, R5, L3, L3, R1, L4, R2, L2, R1, L1, R4, R3, L2, L3, R3, L2, R1, L4, R5, L1, R5, L2, L1, L5, L2, L5, L2, L4, L2, R3';
-const r = day1(dataDay1);
-console.log(r);
+const dayResult = day1(day1Input);
+console.log(dayResult);
