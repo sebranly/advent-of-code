46a7612d5e06a6052f28038ba131afde21b560cc
sebranly <sebranly.pinf@gmail.com>
2018-11-18 12:52:46 -0800

    feat: add day 19 part 2

--- challenges.js
+++ challenges.js
@@ -1150,9 +1150,53 @@ const day19 = (input) => {
 		elfs = otherElfs;
 	}
 
+	const part1 = elfs[0].i;
+
+	let part2;
+	// Naïve implementation (which is too slow) to find a pattern
+	// elfs = [];
+	// for (let i = 0 ; i < input ; i++)
+	// 	elfs.push({ i: i + 1, p: 1 });
+	// let currentIndex = 0;
+	// while (elfs.length > 1) {
+	// 	if (elfs.length % 1000 === 0) console.log(elfs.length);
+	// 	const half = Math.floor(elfs.length / 2);
+	// 	const oppositeIndex = (currentIndex + half) % elfs.length;
+	// 	elfs.splice(oppositeIndex, 1);
+	// 	if (oppositeIndex > currentIndex) {
+	// 		currentIndex++;
+	// 	} else {
+	// 		if (currentIndex >= elfs.length) currentIndex = 0;
+	// 	}
+	// }
+	// part2 = elfs[0].i;
+
+	let powValue = 0;
+	let pow = 0;
+	while (powValue <= input) {
+		pow++;
+		powValue = Math.pow(3, pow);
+	}
+	pow--;
+	powValue = Math.pow(3, pow);
+
+	let inc = 1;
+	let ind = powValue;
+	let res = 0;
+	if (Math.pow(3, pow) === input) {
+		part2 = input;
+	} else {
+		while (ind < input) {
+			if (ind === 2 * powValue) inc = 2;
+			res += inc;
+			ind++;
+		}
+		part2 = res;
+	}
+
 	return {
-		part1: elfs[0].i,
-		part2: -1
+		part1: part1,
+		part2: part2
 	};
 };
 
@@ -1399,7 +1443,23 @@ const data = [
 			{ input: 5, output: 3 },
 			{ input: daysInput[18], output: 1816277 }
 		],
-		part2: []
+		part2: [
+			{ input: 1, output: 1 },
+			{ input: 2, output: 1 },
+			{ input: 3, output: 3 },
+			{ input: 4, output: 1 },
+			{ input: 5, output: 2 },
+			{ input: 6, output: 3 },
+			{ input: 7, output: 5 },
+			{ input: 8, output: 7 },
+			{ input: 9, output: 9 },
+			{ input: 12, output: 3 },
+			{ input: 27, output: 27 },
+			{ input: 500, output: 271 },
+			{ input: 1000, output: 271 },
+			{ input: 10000, output: 3439 },
+			{ input: daysInput[18], output: 1410967 }
+		]
 	}
 ];
 
